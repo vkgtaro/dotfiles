c4e26c1e42aaebbb5bd453093640457a9ee2ac46
komatsu <dkomatsu@oneup-inc.com>
2014-10-27 19:58:11 +0900

    modified to enhance red underline to tab.

--- emacs.d/conf/init-highlighting.el
+++ emacs.d/conf/init-highlighting.el
@@ -9,15 +9,17 @@
 (defface my-face-b-1 '((t (:background "gray"))) nil)
 (defface my-face-b-2 '((t (:background "gray26"))) nil)
 (defface my-face-u-1 '((t (:foreground "SteelBlue" :underline t))) nil)
+(defface my-face-u-2 '((t (:foreground "Red" :underline t))) nil)
 (defvar my-face-r-1 'my-face-r-1)
 (defvar my-face-b-1 'my-face-b-1)
 (defvar my-face-b-2 'my-face-b-2)
 (defvar my-face-u-1 'my-face-u-1)
+(defvar my-face-u-2 'my-face-u-2)
 
 (defadvice font-lock-mode (before my-font-lock-mode ())
   (font-lock-add-keywords
    major-mode
-   '(("\t" 0 my-face-b-2 append)
+   '(("\t" 0 my-face-u-2 append)
      ("　" 0 my-face-b-1 append)
      ("[ \t]+$" 0 my-face-u-1 append)
      ;;("[\r]*\n" 0 my-face-r-1 append)
